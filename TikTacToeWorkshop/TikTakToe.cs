5aa7d493d277e0aa04ba7e620bf212d260a04633
Akash <akashmane12598@gmail.com>
2020-10-10 14:51:09 +0530

    [Akash] Add. UC1 StartGame

---
+++ TikTacToeWorkshop/TikTakToe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TikTacToeWorkshop
+{
+    public class TikTakToe
+    {
+        private char[] board;
+
+        //Initialize borad
+        public void UC1_StartGame()
+        {
+            board = new char[10];
+            for (int position = 1; position < board.Length; position++)
+            {
+                board[position] = ' ';
+            }
+        }
+    }
+}
