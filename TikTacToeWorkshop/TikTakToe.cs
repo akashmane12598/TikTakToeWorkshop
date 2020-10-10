18add9e3729514578b2381872292d931bf74c205
Akash <akashmane12598@gmail.com>
2020-10-10 15:18:42 +0530

    [Akash] Add. UC2 Select X or O

--- TikTacToeWorkshop/TikTakToe.cs
+++ TikTacToeWorkshop/TikTakToe.cs
@@ -8,14 +8,41 @@ namespace TikTacToeWorkshop
     {
         private char[] board;
 
-        //Initialize borad
-        public void UC1_StartGame()
+        public TikTakToe()
         {
             board = new char[10];
+        }
+
+        //Initialize board
+        public void UC1_StartGame()
+        {
             for (int position = 1; position < board.Length; position++)
             {
                 board[position] = ' ';
             }
+            Console.WriteLine("Board has been initialized");
+        }
+        public char UC2_SelectCharacter()//Choose one letter X or O
+        {
+            Console.WriteLine("Select X or O");
+            char userLetter = Convert.ToChar(Console.ReadLine());
+            char compLetter = ' '; 
+
+            if (userLetter == 'X')
+            {
+                compLetter = 'O';
+            }
+            else if (userLetter == 'O')
+            {
+                compLetter = 'X';
+            }
+            else
+            {
+                Console.WriteLine("Invalid Character");
+            }
+            Console.WriteLine("User Selected: " + userLetter);
+            return compLetter;
+
         }
     }
 }
