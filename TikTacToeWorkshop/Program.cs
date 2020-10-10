372e1160de87863f992c1ba6b584993feee9a041
Akash <akashmane12598@gmail.com>
2020-10-10 14:49:16 +0530

    [Akash] Add. UC1 StartGame

--- TikTacToeWorkshop/Program.cs
+++ TikTacToeWorkshop/Program.cs
@@ -7,6 +7,8 @@ namespace TikTacToeWorkshop
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to TikTakToe Game");
+            TikTakToe tk = new TikTakToe();
+            tk.UC1_StartGame();
         }
     }
 }
